b0a89e57276c8ec59ccedaf734ee30675303fbcf
martindupraz <martin.d@wisecx.com>
2025-05-19 19:40:37 -0300

    Bucles y listas

---
+++ listas.py
@@ -0,0 +1,30 @@
+
+
+#------------------------------
+# LISTA [ Colección de elementos mutables y ordenados]
+#------------------------------
+
+#nombre
+frutas = ["Manzana", "Plátano", "Pera", "Mandarina", "Fresa", "Piña"]
+
+# bucle for
+# for fruta in frutas:
+#     print(fruta)
+
+# # bucle for con indice disponible
+# for i in range(len(frutas)):
+#     print(frutas[i])
+#     print(i)
+
+# bucle whilw
+# i = 0
+# while i < len(frutas):
+#     print(frutas[i])
+#     i += 1
+
+# shorthand (abreviación)
+[print(fruta) for fruta in frutas]
+
+#------------------------------
+# COMPRENSION DE LISTAS
+#------------------------------
